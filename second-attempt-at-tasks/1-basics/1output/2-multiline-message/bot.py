0664a1644433a681ce62fc9a47c695929dc2fd6c
CharlesBasham132 <charliefred132@gmail.com>
2019-10-14 14:37:32 +0000

    added multiline strings

--- second-attempt-at-tasks/1-basics/1output/2-multiline-message/bot.py
+++ second-attempt-at-tasks/1-basics/1output/2-multiline-message/bot.py
@@ -1,3 +1,21 @@
 #display message to the standard output
 print ("system failure imminent")
 print ("system restoration rebbot has been initiated")
+print () #This prints a blank line, no "" speach marks are needed due to it not being a text string
+print ("...rebooting sensory system")
+print ("rebooting output motors")
+print ("rebooting hover engine")
+print ()
+print ("system online")
+print ()
+# An alternative approach is to use a long string. This is used with the addition of three """" quotation marks
+print("""
+System Failure Imminent!
+System reboot has been initiated...
+
+...rebooting sensory system
+...rebooting output motors
+...rebooting hover engine
+
+System online.
+""")

10203483fea2bc5ae367050593e498b9ef88736d
CharlesBasham132 <charliefred132@gmail.com>
2019-10-18 12:06:56 +0000

    user inputed eyes program added

---
+++ second-attempt-at-tasks/1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,8 @@
+#ask the user what text character they would wish to be the robots eyes
+print("enter character symbol for eyes")
+eyes = input()
+print("#########")
+print("#       #")
+print("# ",eyes,eyes, " #")
+print("# ----- #")
+print("#########")

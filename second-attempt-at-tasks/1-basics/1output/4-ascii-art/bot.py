56de512b152139e13245e41202ab4a2a9b693e98
CharlesBasham132 <charliefred132@gmail.com>
2019-10-18 10:45:07 +0000

    added ascii art robot face and long string ascii art robot face

---
+++ second-attempt-at-tasks/1-basics/1output/4-ascii-art/bot.py
@@ -0,0 +1,19 @@
+#display an ascii art robot face
+print("##########")
+print("#        #")
+print("#  *  *  #")
+print("#        #")
+print("# ****** #")
+print("#        #")
+print("##########")
+
+#display same face in a long string
+print("""
+##########
+#        #
+#  *  *  #
+#        #
+# ****** #
+#        #
+##########
+""")

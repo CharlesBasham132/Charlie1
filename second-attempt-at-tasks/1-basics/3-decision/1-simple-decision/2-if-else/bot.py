fe764ce32272600f8fb844a76f682d12bc66b12f
CharlesBasham132 <charliefred132@gmail.com>
2019-10-25 08:54:05 +0000

    completed if-else task

---
+++ second-attempt-at-tasks/1-basics/3-decision/1-simple-decision/2-if-else/bot.py
@@ -0,0 +1,12 @@
+
+
+print("enter activity to be performed")
+bot_activity = str(input())
+
+if (bot_activity == "calculate"):
+    print("performing calculations...")
+
+else:
+    print("performing activity...")
+
+print("activity completed")

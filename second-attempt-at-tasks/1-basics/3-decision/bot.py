2a81b3a72f04996b736c47fa2225bfe2eb55219a
CharlesBasham132 <charliefred132@gmail.com>
2019-10-25 08:39:00 +0000

    completed first if statement task

---
+++ second-attempt-at-tasks/1-basics/3-decision/bot.py
@@ -0,0 +1,9 @@
+#ask the user to input the book genre
+print("what genre of book is that?")
+book_genre = str(input())
+
+if (book_genre == "adventure"):
+    print("I like adventure books")
+
+print("finished reading book")
+

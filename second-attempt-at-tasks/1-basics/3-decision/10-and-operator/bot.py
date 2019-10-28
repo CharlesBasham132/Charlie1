490bf0921401979e94d208f09fe8b09aefd728b2
CharlesBasham132 <charliefred132@gmail.com>
2019-10-28 16:05:07 +0000

    completed and-operator task

---
+++ second-attempt-at-tasks/1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,10 @@
+
+print("what did I hear?")
+sound = str(input())
+print("what did I see?")
+sight = str(input())
+if ((sound == "grrr") and (sight == "two red eyes")):
+    print("I should get out of here!")
+
+else:
+    print("I will keep going...")

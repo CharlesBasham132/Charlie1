8c970e1e17cfa7a1cba7d178024503aca59f0287
CharlesBasham132 <charliefred132@gmail.com>
2019-10-28 18:29:15 +0000

    completed 1-simple loop task

--- second-attempt-at-tasks/1-basics/4-repetition/while-loop/1-simple/bot.py
+++ second-attempt-at-tasks/1-basics/4-repetition/while-loop/1-simple/bot.py
@@ -1,9 +1,11 @@
-print("the following is a while loop\n")
-first_count = 0
-while (first_count <=10):
-    print (first_count)
-    first_count = first_count +1
-    second_count = 0
-while (second_count <=4):
-        print(second_count)
-        second_count =second_count +1
+#ask the user to input the amount of cables to be removed
+print("how many cables should i remove?")
+cable_number = int(input())
+
+#create the cable count variable
+cable_count = 0
+#create the while loop. the loop will repeat and print "cable removed" until the count variable matches the cable number variable inputed by the user.
+while (cable_count < cable_number):
+    print("cable removed")
+    cable_count = cable_count + 1
+print("all cables are removed now")

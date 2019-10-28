5ddac743eed5775198b12442254946a3b1bc2bd8
CharlesBasham132 <charliefred132@gmail.com>
2019-10-28 18:12:11 +0000

    started loops task

---
+++ second-attempt-at-tasks/1-basics/4-repetition/while-loop/1-simple/bot.py
@@ -0,0 +1,9 @@
+print("the following is a while loop\n")
+first_count = 0
+while (first_count <=10):
+    print (first_count)
+    first_count = first_count +1
+    second_count = 0
+while (second_count <=4):
+        print(second_count)
+        second_count =second_count +1

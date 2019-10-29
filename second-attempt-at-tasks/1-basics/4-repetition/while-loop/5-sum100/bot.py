50ef9778d7e8b4597abfbbd4eb1ac3862311bace
CharlesBasham132 <charliefred132@gmail.com>
2019-10-29 12:39:09 +0000

    finished 5-sum task

---
+++ second-attempt-at-tasks/1-basics/4-repetition/while-loop/5-sum100/bot.py
@@ -0,0 +1,13 @@
+
+print("calculating the sum of the first 100 numbers:")
+
+count=1
+total = 0
+while (count <= 100):
+    total = total +count
+    count = count +1
+
+
+
+
+print("finished, the awnser is:",total)

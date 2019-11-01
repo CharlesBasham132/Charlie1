a713c90dac649c9955905a4bb3b103293699b353
CharlesBasham132 <charliefred132@gmail.com>
2019-11-01 12:07:54 +0000

    done

--- second-attempt-at-tasks/1-basics/4-repetition/2-for-loop/1-simple/bot.py
+++ second-attempt-at-tasks/1-basics/4-repetition/2-for-loop/1-simple/bot.py
@@ -1,7 +1,9 @@
 print("how many mountains should i display?")
 mountain_number = int(input())
+print("displaying...:")
 for count in range(0, mountain_number, 1):
     print("""
       /\\
      /  \\
     /    \\ """)
+print("displaying complete.")

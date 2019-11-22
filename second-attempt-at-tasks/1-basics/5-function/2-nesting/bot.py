755487803b6da48d7b2c14bff05882148c182315
CharlesBasham132 <charliefred132@gmail.com>
2019-11-22 10:53:39 +0000

    completed 5-function 2-nesting tast

---
+++ second-attempt-at-tasks/1-basics/5-function/2-nesting/bot.py
@@ -0,0 +1,8 @@
+def ask_vision():
+    print("what is infront of us?")
+    sight = str(input())
+    if sight == ("a large boulder"):
+        print("It's time to run")
+    else:
+        print("we will be fine")
+ask_vision()

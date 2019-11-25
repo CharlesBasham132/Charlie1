a9258d6c64ffb5d98b258d081c13bd5b2e4563e0
CharlesBasham132 <charliefred132@gmail.com>
2019-11-25 15:31:22 +0000

    completed task

--- second-attempt-at-tasks/1-basics/5-function/4-loop/bot.py
+++ second-attempt-at-tasks/1-basics/5-function/4-loop/bot.py
@@ -1,9 +1,10 @@
+
 def bridge_crossing(number_of_steps):
     for step in range(number_of_steps):
         print("crossed step: ")
     
     if (number_of_steps>5):
-        print("the bridge is collapsing")
+        print("the bridge is collapsing!!!!!")
     else:
         print("we should keep going")
 

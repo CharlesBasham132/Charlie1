b6968adeb6c8c7461e8f986e061e7053494cdce9
CharlesBasham132 <charliefred132@gmail.com>
2019-11-25 15:52:02 +0000

    completed 5-multiple_parameters

---
+++ second-attempt-at-tasks/1-basics/5-function/5-multiple_parameters/bot.py
@@ -0,0 +1,8 @@
+def climb_ladder(steps_remaining, seps_crossed):
+    if (steps_remaining > seps_crossed):
+        print("Still some way to go")
+    else:
+        print("We are nearly there!")
+    
+climb_ladder (5,2)
+climb_ladder (2,10)
